93afed73e516577b57675bf7aa4a5cc959dfb63f
Senseytreyser <senseytreyser@mail.ru>
2019-03-24 20:20:03 +0300

    Добавление функции render
    Переработка класса filter и добавление функции, которая формирует блок с кнопками.

--- js/filer.js
+++ js/filer.js
@@ -1,10 +1,46 @@
-function Filter(){
+function Filter(myId, myClass){
+    this.id = myId;
+    this.class = myClass;
     this.content = [];
 }
 
 Filter.prototype.addRow = function (row){
     var row = row;
     this.content.push(row)
+}
+
+Filter.prototype.render = function(){
+    
+    //Создание главного блока
+    var container = document.createElement("div");
+    container.id = this.id; 
+    container.className = this.class; 
+
+    var addButtField = document.createElement("div");
+    addButtField.className = "button-fieldset";
+
+    var addButton = document.createElement("button");
+    addButton.className = "add-button";
+    addButton.innerHTML = "add contribution";
+
+    var actButtField = document.createElement("div");
+    actButtField.className = "action-fieldset";
+
+    var applyButton = document.createElement("button");
+    applyButton.className = "apply-button";
+    applyButton.innerHTML = "apply";
+
+    var clearButton = document.createElement("button");
+    clearButton.className = "clear-button";
+    clearButton.innerHTML = "clear filter";
+
+    addButtField.appendChild(addButton);
+    actButtField.appendChild(applyButton);
+    actButtField.appendChild(clearButton);
+    container.appendChild(addButtField);
+    container.appendChild(actButtField);
+
+    return container;
 }
 
 Filter.prototype.showInfo = function (){
