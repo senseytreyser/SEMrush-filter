bd1f09b8f37896f56952c3fe6632f8d8bad0299b
Senseytreyser <senseytreyser@mail.ru>
2019-03-25 13:54:10 +0300

    Поведение выпадашек
    Добавлено поведение к выпадашкам. При изменении первой выпадашки, меняется состояние у элементов второй выпадашки.
    Прописаны значения по умолчанию.

--- js/filter-row.js
+++ js/filter-row.js
@@ -3,9 +3,6 @@ function FilterRow(type,operation,value){
     this.typeList = ["Text field", "Number field"];
     this.opForNumList = ["Equal","Greater than","Less than"];
     this.opForTextList = ["Containing","Exactly matching","Begins with","Ends withText"]
-    //this.value = value || "";
-    //this.operation = operation || "Containing";
-    //this.type = type || "Text field";
 
 }
 
@@ -17,7 +14,8 @@ FilterRow.prototype.render = function(){
 
     //Создание трёх полей для ввода
     var selectType = document.createElement("select");
-    selectType.className = "select-tipe";
+    selectType.className = "select-type";
+    selectType.addEventListener("change", changeOperations);
 
     var selectOperation = document.createElement("select");
     selectOperation.className = "select-operation";
@@ -25,6 +23,11 @@ FilterRow.prototype.render = function(){
     var inputValue = document.createElement("input");
     inputValue.className = "input-value";
     inputValue.setAttribute("type","text");
+
+    //Создание блока с крестиком
+    var cross = document.createElement("div");
+    cross.className = "cross hidden";
+    cross.innerHTML = "X";
 
     //Заполнение первой выпадашки элементами
     var elem;
@@ -46,15 +49,15 @@ FilterRow.prototype.render = function(){
     //Заполнение той же выпадашки элементами из второго массива
     for (var i = 0; i < this.opForNumList.length; i++){
         elem = document.createElement("option");
-        elem.className = "select-operation__value num-value";
+        elem.className = "select-operation__value num-value hidden";
         elem.innerText = this.opForNumList[i];
-        elem.setAttribute("hidden","true");
         selectOperation.appendChild(elem);
     }
 
     fieldset.appendChild(selectType);
     fieldset.appendChild(selectOperation);
     fieldset.appendChild(inputValue);
+    fieldset.appendChild(cross);
 
     return fieldset;
 }
@@ -66,3 +69,41 @@ FilterRow.prototype.getRowObj = function(){
     }
 }
 
+function changeOperations(e){
+    var target = e.target;
+    var selectOperation = target.nextElementSibling;
+    
+    //Скрытие одних эелементов и открытие других
+    var operationList = selectOperation.childNodes;
+    var elem;
+    for (var i = 0; i < operationList.length; i++){
+        elem = operationList[i];
+        elem.classList.toggle("hidden");
+    }
+    
+    //Установка значения втоорой выпадашки на значение по умолчанию, соответствующего типа
+    var firstSelectValue = target.value;
+    var text = "Text field";
+    var number = "Number field";
+    if (firstSelectValue === text){
+        operationList[0].setAttribute("selected","true");
+        operationList[4].removeAttribute("selected");
+    }
+    if (firstSelectValue === number){
+        operationList[4].setAttribute("selected","true");
+        operationList[0].removeAttribute("selected");
+    }
+
+
+    //Изменение типа поля ввоода
+    var input = selectOperation.nextElementSibling;
+    var type = input.getAttribute("type");
+    if (type === "text"){
+        input.setAttribute("type","number");
+        input.value = "";
+    } else if (type === "number"){
+        input.setAttribute("type","text");
+        input.value = "";
+    }
+
+}
